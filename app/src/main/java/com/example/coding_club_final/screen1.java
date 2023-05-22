e239965ca88bfaa9ed11cf64d4f2b191d63be5c6
Redluigi1 <ayush171003@gmail.com>
2023-05-22 15:30:12 +0530

    navigation drawer

--- app/src/main/java/com/example/coding_club_final/screen1.java
+++ app/src/main/java/com/example/coding_club_final/screen1.java
@@ -1,9 +1,15 @@
 package com.example.coding_club_final;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 
 public class screen1 extends AppCompatActivity {
@@ -15,6 +21,15 @@ public class screen1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_screen1);
+        MaterialToolbar toolbar = findViewById(R.id.topAppbar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 
     }
 }
